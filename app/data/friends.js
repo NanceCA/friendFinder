98621910975f4202f485c18dbdd7239673c68625
nance <48306129+NanceCA@users.noreply.github.com>
2019-06-22 17:48:49 -0700

    successful compatability for loop

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,37 @@
 
 
-var friends = [];
+var friends = [{
+    name: "Violet",
+    picture: "Link",
+    scores: [
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5
+    ]
+},
+{
+    name: "Gus",
+    picture: "Link",
+    scores: [
+        5,
+        1,
+        4,
+        3,
+        2,
+        1,
+        5,
+        3,
+        4,
+        2
+    ]
+}
+];
 
 module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -13,27 +13,57 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         //send this to app/data/friends.js to save as an array of objects
-        //push into the array friends 
-
         var newFriend = (req.body);
-        console.log("The req.body is", newFriend);
+        //determining the final score of the user
+
+        // //declaring function for reduce method
+        // var adder = (accumulator, currentValue) => {
+        //     return accumulator + currentValue;
+        // }
+
+        //first for loop here
+        var compatability = [];
+        for (var i = 0; i < friends.length; i++) {
+            //empty array to capture absolute value difference
+            var differences = [];
+            //empty array to capture compatability scores
+
+
+            console.log("Comparing ", newFriend.name, "and ", friends[i].name);
+
+            //nested for loop to calculate scores
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var diff = Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriend.scores[j]));
+                differences.push(diff);
+            }
+
+            console.log("The absolute value array for ", newFriend.name, "and ", friends[i].name, differences);
+            reducedscore = differences.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+            console.log("reduced score", reducedscore);
+            compatability.push(reducedscore);
+            // var reducedscore = 0;
+            // for (var k = 0; k < differences.length; k++) {
+            //     reducedscore += parseInt(differences[k]);
+            // }
+
+            console.log("The reduced score for ", friends[i].name, "and ", newFriend.name, "is ", reducedscore);
+        }
+
+        console.log("Compatability array ", compatability);
+
+        var compatible = Math.min.apply(null, compatability);
+        console.log("minimum of the array is ", compatible);
+        var match = compatability.indexOf(compatible);
+        // var realMatch = friends[match].name;
+        // console.log(realMatch);
+
+        //COMPATABILITY SUCCESS!
+        console.log("You are most compatible with ", friends[match].name);
+
+        //push into the array friends
         friends.push(newFriend);
         res.json(friends)
 
-        //loop through the list to find a compatible friend
-
-        // var totalScore = 0;
-        // for (var i = 0; i < friends.length; i++) {
-
-        //     for (var j = 1; j < friends.length; j++) {
-
-        //         totalScore = [friends[i].question1 - friends[j].question1];
-        //         if (totalScore === 0) {
-        //             alert(friends[i].name, "is your most compatible friend!");
-        //         }
-
-        //     }
-        // }
     });
 
 }
